8e1ec28df3a595c11fc73fe2db4c0755b7200b21
Jack Yu <bjack617@gmail.com>
2014-01-08 17:16:03 +0800

    - 修改 bower 套件安裝目錄路徑 - 利用 bower 安裝 bootstrap 和 angular-ui-bootstrap 套件 - 修改 server 端的 favicon 對應路由 - 建立 index.html 和 app.js 基礎架構

---
+++ client/src/app.js
@@ -0,0 +1,4 @@
+var app = angular.module('resourceApp', [
+	'ngRoute',
+	'ngResource'
+]);
--- server/routes/static.js
+++ server/routes/static.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 exports.addRoutes = function (app, config) {
-	app.use(express.favicon(config.server.distFolder + '/favicon.ico'));
+	app.use(express.favicon(config.server.distFolder + '/assets/images/favicon.ico'));
 
 	app.use(config.server.staticUrl, express.static(config.server.distFolder));
 	app.use(config.server.vendorUrl, express.static(config.server.vendorFolder));
